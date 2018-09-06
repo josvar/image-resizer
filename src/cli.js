8a7c7139963335a7ab7e82af1fde95212f385fe6
Josue Vargas <jvasquez@digbang.com>
2018-09-06 12:58:56 -0300

    Add default params.

--- src/cli.js
+++ src/cli.js
@@ -39,31 +39,20 @@ program
   .option('-i, --input <path>', 'Input file or folder')
   .option('-o, --output <path>', 'Output file or folder')
   .action((trash, options) => {
-    let config = options.config
-    if (!config) {
-      console.error(`Missing config [-c] option, aborting.`)
-      process.exit(1)
+    const defaultOptions = {
+      config: './images.config.js',
+      input: './input',
+      output: './output',
+      preset: 'default',
     }
-    config = require(path.resolve(options.config))
 
-    const preset = options.preset
-    if (!preset) {
-      console.error(`Missing preset [-p] option, aborting.`)
-      process.exit(1)
-    }
+    const configPath = options.config || defaultOptions.config
+
+    const config = require(path.resolve(configPath))
+    const preset = options.preset || defaultOptions.preset
     const presetConfig = config.presets[preset]
-
-    const input = options.input
-    if (!input) {
-      console.error(`Missing input [-i] option, aborting.`)
-      process.exit(1)
-    }
-
-    const output = options.output
-    if (!output) {
-      console.error(`Missing output [-o] option, aborting.`)
-      process.exit(1)
-    }
+    const input = options.input || defaultOptions.input
+    const output = options.output || defaultOptions.output
 
     if (!presetConfig) {
       console.error(`Preset ${preset} does not exists, aborting.`)
